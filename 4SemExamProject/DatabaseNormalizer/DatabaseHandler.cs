658e7052e0a3c8a656331e2b6c293b179a302d8a
DevAndersen <martinba.private@gmail.com>
2017-06-02 23:31:22 +0200

    Same as previous commit, this time (hopefully) with all the intended files.

---
+++ 4SemExamProject/DatabaseNormalizer/DatabaseHandler.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseNormalizer
+{
+    public class DatabaseHandler
+    {
+        public List<Dictionary<string, double>> GetNormalizedData(string databaseLocation, string[] columns, string queryConditions, double normalizationMinimum, double normalizationMaximum)
+        {
+            List<string[]> dataFromDatabase = GetDataFromDatabase(databaseLocation, columns, queryConditions);
+
+            return NormalizeData(dataFromDatabase, columns, normalizationMinimum, normalizationMaximum);
+        }
+
+        private List<string[]> GetDataFromDatabase(string databaseLocation, string[] columns, string queryConditions)
+        {
+            SqlConnection connection = new SqlConnection()
+            {
+                ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"" + databaseLocation + "\";Integrated Security=True;Connection Timeout=600;"
+            };
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            string columnList = "";
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                columnList += columns[i] + (i == columns.Length - 1 ? "" : ", ");
+            }
+
+            cmd.CommandText = "SELECT " + columnList + " FROM moviedb " + queryConditions;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = connection;
+
+            connection.Open();
+
+            reader = cmd.ExecuteReader();
+
+            List<string[]> resultStrings = new List<string[]>();
+
+            while (reader.Read())
+            {
+                string[] strings = new string[columns.Length];
+
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    object data = reader[columns[i]];
+                    string dataString = "";
+
+                    if (data.ToString().Contains('|'))
+                    {
+                        dataString = reader[columns[i]].ToString().Split('|')[0];
+                    }
+                    else
+                    {
+                        dataString = reader[columns[i]].ToString();
+                    }
+
+                    strings[i] = dataString;
+                }
+                string[] resultString = new string[columns.Length];
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    resultString[i] = strings[i];
+                }
+                resultStrings.Add(resultString);
+            }
+            reader.Close();
+            connection.Close();
+
+            return resultStrings;
+        }
+
+        private List<Dictionary<string, double>> NormalizeData(List<string[]> dataFromDatabase, string[] columns, double normalizationMinimum, double normalizationMaximum)
+        {
+            List<Dictionary<string, double>> columnDictionaries = new List<Dictionary<string, double>>();
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                int counter = 0;
+                Dictionary<string, double> dictionary = new Dictionary<string, double>();
+                foreach (string[] resultString in dataFromDatabase)
+                {
+                    string result = resultString[i];
+                    if (!dictionary.ContainsKey(result))
+                    {
+                        dictionary.Add(result, counter);
+                        counter++;
+                    }
+                }
+                columnDictionaries.Add(dictionary);
+            }
+
+            foreach (Dictionary<string, double> dictionary in columnDictionaries)
+            {
+                for (int i = 0; i < dictionary.Count; i++)
+                {
+                    string key = dictionary.ElementAt(i).Key;
+
+                    double normalizedValue = normalizationMinimum + ((double)i / (dictionary.Count - 1)) * (normalizationMaximum - normalizationMinimum);
+
+                    dictionary[key] = normalizedValue;
+                }
+            }
+
+            return columnDictionaries;
+        }
+    }
+}
--- 4SemExamProject/NeatConsole/Program.cs
+++ 4SemExamProject/NeatConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using NeatLib;
+﻿using DatabaseNormalizer;
+using NeatLib;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -34,44 +35,6 @@ namespace NeatConsole
             Console.WriteLine("1 XOR 1 = " + Math.Round(ann.Execute(new double[] { 1, 1 })[0], 2));
 
             Console.ReadLine();
-
-            SqlConnection connection = new SqlConnection()
-            {
-                ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Martin\\4SemExamProject.mdf\";Integrated Security=True;Connection Timeout=600;"
-            };
-
-            SqlCommand cmd = new SqlCommand();
-            SqlDataReader reader;
-
-            cmd.CommandText = "SELECT movie_title, genres, actor_2_name, actor_3_name, director_name, budget, gross FROM moviedb WHERE actor_1_name = 'Robert Downey Jr.'";
-            cmd.CommandType = CommandType.Text;
-            cmd.Connection = connection;
-
-            connection.Open();
-
-            reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-                string[] strings =
-                {
-                    reader["movie_title"].ToString(),
-                    reader["genres"].ToString().Split('|')[0],
-                    //reader["actor_2_name"].ToString(),
-                   // reader["actor_3_name"].ToString(),
-                    reader["director_name"].ToString(),
-                    reader["budget"].ToString(),
-                    reader["gross"].ToString(),
-                };
-                string queryString = "";
-                for (int i = 0; i < strings.Length; i++)
-                {
-                    queryString += strings[i] + (i == strings.Length - 1 ? "" : ", ");
-                }
-                Console.WriteLine(queryString);
-            }
-            reader.Close();
-            connection.Close();
 
             new Program().Run();
             Console.ForegroundColor = ConsoleColor.Green;
@@ -117,6 +80,24 @@ namespace NeatConsole
                 }
             }
             Console.WriteLine();
+        }
+
+        private List<Dictionary<string, double>> GetDataFromDatabase()
+        {
+            string[] columns = new string[]
+            {
+                //"movie_title",
+                "genres",
+                "director_name",
+                "budget",
+                "gross"
+            };
+
+            string condition = "WHERE actor_1_name = 'Robert Downey Jr.'";
+            string databaseLocation = AppDomain.CurrentDomain.BaseDirectory;
+            databaseLocation = databaseLocation.Replace(@"bin\Debug\", @"database\4SemExamProject.mdf");
+            DatabaseHandler dbh = new DatabaseHandler();
+            return dbh.GetNormalizedData(databaseLocation, columns, condition, 0, 1);
         }
     }
 }
